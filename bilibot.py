d89b20dc63011f55c2b7adb917e3ee71808e3d32
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-04-08 22:30:16 +0800

    Update bilibot.py

--- bilibot.py
+++ bilibot.py
@@ -4,4 +4,4 @@ import urllib.request
 keyword = input("请输入视频BV号:")
 url = "https://www.bilibili.com/video/BV" + urllib.parse.quote(keyword)
 response = urllib.request.urlopen(url)
-print(response.read().decode('utf-8'))
+print(response.read().decode("utf-8"))
