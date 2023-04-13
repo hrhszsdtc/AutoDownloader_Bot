0449f8934b27e76f7a70e9f062c145ba2c2085f6
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-04-13 21:38:59 +0800

    test

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-# BiliDownloader_Bot
-一个解析并下载Bilibili视频的Python爬虫,目前编写正在进行中,且有时可能会在线下测试,Github上会有一定滞后.
-
-## 编码规范
-
-### Python编码规范
-本仓库中的Python代码请严格遵照[PEP 8文档](https://peps.python.org/pep-0008/)以及[Google Python Style Guide](https://google.github.io/styleguide/pyguide.html).包含未按照代码规范编写的Python代码的PR会被无理由Close(事实上只要代码没问题作者还是会merge并之后帮你改一下的).  
-对于非Python程序并无硬性规定,但最好可以遵守[Google Style Guide](https://google.github.io/styleguide/).  
-此外,本仓库对于是否有注释并无特殊要求,看个人喜好;若有,则应尽量遵守相关PEP文档的规定.
-
-## 联系作者
-E-mail:`hrhszsdtc@gmail.com`
-
-## 捐助
-BTC:`3J5DfkjrHLxLRwMyeNHfhDd3GxTAo8yRAJ`
-
-## 免责声明
-仅供学习使用,滥用后果自负!
+# 协作者测试分支
---
+++ bot.py
@@ -0,0 +1,94 @@
+import urllib.parse
+import urllib.request
+from urllib import request
+
+import chardet  # pip install chardet
+import requests
+
+
+def get_request(url, flag, *file_root):
+    '''
+
+
+    用于http/https协议下的get请求
+
+
+    url代表要请求的url,flag为返回方式:
+
+
+            若flag为0,则返回明文或2进制数据
+
+
+            若flag为1,则将得到的数据写入文件
+
+
+    file_root为选填参数,代表请求的数据储存是时的目录,默认为'./'
+    '''
+
+    try:
+
+        response = request.urlopen(url)
+
+    except:
+
+        print('[!]请求URL下的数据异常')
+
+        return -1  # 将函数以异常形式返回错误代
+
+    response = requests.get(url)
+    data = response.content
+    result = chardet.detect(data)
+    encoding = result["encoding"]
+    content = data.decode(encoding, errors='replace')
+
+    if flag == 0:  # 以明文或数据输出并且返回
+
+        print(content)  # 打印数据
+
+        return content  # 返回数据
+
+    elif flag == 1:  # 将数据写入文件
+        root = ''
+
+        if file_root == None:  # 如果未传递参数
+
+            root = './'  # 设置为默认目录
+
+        else:  # 如果传入参数
+
+            root = file_root  # 设置目录
+
+        # 获得文件名
+        '''
+
+		pass
+
+		'''
+
+        # 写入文件
+
+        file = open(root+filename, 'w')  # 以写的方式打开文件
+
+        file.write(data)  # 写入数据
+
+        file.close()  # 关闭文件
+
+        return 0
+
+
+def main():
+
+    # 获得视频BV号
+
+    keyword = '1Na4y1T75k'  # input("请输入视频BV号:")
+
+    # 合成为目标URL
+
+    url = f'https://www.bilibili.com/video/BV{keyword}/'
+
+    get_request(url, 0)
+
+
+if __name__ == '__main__':
+
+    main()
