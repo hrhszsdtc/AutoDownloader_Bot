ec877215dd22dfbe7e2eb9053b20d7f96aadd632
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-04-14 19:36:52 +0800

    Update bili_bot.py

--- bili_bot.py
+++ bili_bot.py
@@ -1,15 +1,24 @@
 import random
 import urllib.parse
 import urllib.request
+import socket
+import socks # pip install PySocks
 
 from bs4 import BeautifulSoup
 
+
+# useragent
 user_agents = []
 with open('./conf/user_agents.txt') as f:
     lines = f.readlines()
     for line in lines:
         user_agents.append(line.strip('\n'))
 user_agent = random.choice(user_agents)
-print(user_agent)
+
+# proxy(needs tor service)
+socks.set_default_proxy(socks.SOCK5, "localhost", 9150)
+socket.socket = socks.socksocket
+
+
 keyword = input("请输入视频BV号:")
 url = f'https://www.bilibili.com/video/BV{keyword}/'
