302cda037c5a80f713c0aad4cf059fa5238c0307
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-04-14 19:20:37 +0800

    test

--- bili_bot.py
+++ bili_bot.py
@@ -1,7 +1,15 @@
+import random
 import urllib.parse
 import urllib.request
 
+from bs4 import BeautifulSoup
+
+user_agents = []
+with open('./conf/user_agents.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        user_agents.append(line.strip('\n'))
+user_agent = random.choice(user_agents)
+print(user_agent)
 keyword = input("请输入视频BV号:")
 url = f'https://www.bilibili.com/video/BV{keyword}/'
-response = urllib.request.urlopen(url)
-print(response.read().decode('utf-8'))
