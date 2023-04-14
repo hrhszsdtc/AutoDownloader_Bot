aa69860435dce603d5cca118b9c2fe30426fd7b5
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-04-14 18:48:55 +0800

    Delete bilibot.py

--- bilibot.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-import urllib.request
-
-keyword = input("请输入视频BV号:")
-url = f'https://www.bilibili.com/video/BV{keyword}/'
-response = urllib.request.urlopen(url)
-print(response.read().decode('utf-8'))
