d5bcc9a79d606529cec3cbfce2164328cd62eeca
ZZH <123837353+ZeHangZhu@users.noreply.github.com>
2023-05-02 01:19:20 +0800

    Update start.py

--- start.py
+++ start.py
@@ -4,7 +4,7 @@
 #by ZZH
 
 #支持爬取的域名,注:全小写!!!
-COULD_DOMAIN = ['baidu.com']
+COULD_DOMAIN = ['baidu.com','www.baidu.com','v.qq.com']
 
 ZZH="\
       __________\t  __________\t       ___    ___\n\
@@ -25,6 +25,7 @@ import urllib
 import bs4
 from time import sleep
 import re
+from std_output import *	#软件标准输出库
 
 def get_request(url, flag, *file_root):
     '''
@@ -38,7 +39,7 @@ def get_request(url, flag, *file_root):
     try:
         response = request.urlopen(url)
     except:
-        print('[!]请求URL下的数据异常')
+        pwarm('请求URL下的数据异常')
         return -1  # 将函数以异常形式返回错误代
 
     response = requests.get(url)  # 获取响应
@@ -70,7 +71,8 @@ def get_request(url, flag, *file_root):
         return 0
 
 def un_pack(url):
-	print(f"\n[*]正在解析:[{url}]")
+	print('\n')
+	pout(f"正在解析:[{url}]")
     
 	#解析url
 	temp = url[0:10]
@@ -94,8 +96,9 @@ def un_pack(url):
 	print(f'    domain:{domain}')
 
 	#查询是否支持爬取
-	if domain in COULD_DOMAIN == False:
-		print(f'[!]抱歉,该域名下[({doamin}) from ({url})的资源暂时不支持爬取!')
+	if (domain in COULD_DOMAIN) == False:
+		pwarm(f'报歉,该域名下[({domain}) from (\
+{url})的资源暂时不支持爬取!')
 		return
     #获取域名
 
@@ -107,7 +110,9 @@ def main(mode,*url):
 		url = ''
 
 		#主界面
-		print(f'{cutline}\n\n\n{ZZH}\n\n\n{cutline}\n')		#打印ZZH图标
+		print(f'{cutline}\n\n\n\
+{ZZH}\n\n\n\
+{cutline}\n')		#打印ZZH图标
 		print('\t:)Tip:输入exit退出,输入url地址开始爬取')
 		while True:
 			flag = 1
@@ -117,22 +122,23 @@ def main(mode,*url):
 				return 0			#正常退出
 			
 			#检查URL是否可用
-			print(f'[*]Checking url[{url}]')
+			pout(f'Checking url[{url}]')
 			
 			try:
 				respnse = urllib.request.urlopen(url)
 			
 			#如果异常
 			except urllib.request.URLError as e:
-				print(f"\033[0;33m[!]URL不可用!\n\033[0m\033[0;31m  :){e}\033[0m")
+				print(f"\033[0;33m[!]URL不可用!\n\033[0m\
+\033[0;31m  :){e}\033[0m")
 				flag = 0
 			except Exception as e:
-				print(f'---------------------------\033[0;31mErrors\033[0m---------------------------\n\t\033[0;31m{e}\033[0m\n\n')
+				error(e)
 				flag = 0
 			#如果可用
 			if flag == 1:
 				
-				print('[ \033[0;32mOK\033[0m ] URL Checking Over')
+				pok('[ \033[0;32mOK\033[0m ] URL Checking Over')
 				
 				#爬取
 				print('[*]Spider Start-up')
@@ -145,8 +151,12 @@ if __name__ == '__main__':
 	try:
 		if main(0) == None:
 			print("\f\f:)程序非正常退出,可能是崩溃了!")
-			print('\t请向ZZH20081023@163.com发送标题为"Bug Report"的邮件,并复制报错信息以及崩溃前的具体操作,感谢您的反馈!')
+			print('请向ZZH20081023@163.com发送标题为"Bu\
+g Report"的邮件,并复制报错信息以及崩溃前的具体操作,感谢您\
+的反馈!')
 	except Exception as e:
 		print(f"{e}\f\f:)程序非正常退出,可能是崩溃了!")
-		print('\t请向ZZH20081023@163.com发送标题为"Bug Report"的邮件,并复制报错信息以及崩溃前的具体操作,感谢您的反馈!')
-#未完成
+		print('请向ZZH20081023@163.com发送标题为"Bu\
+g Report"的邮件,并复制报错信息以及崩溃前的具体操作,感谢您\
+的反馈!')
+	
