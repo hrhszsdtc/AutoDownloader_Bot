2aae7855db2d7f2743e84ef211995e71db6b1e49
ZZH <123837353+ZeHangZhu@users.noreply.github.com>
2023-05-02 01:25:56 +0800

    Update std_output.py

--- std_output.py
+++ std_output.py
@@ -1,23 +1,7 @@
 #! usr/bin/python3
+#代码灰常简单，注释就不写了
 
 def pout(str):
 	print(f"[*]\033[0;33m{str}\033[0m")
 
 def pwarm(str):
-	print(f"\033[0;31m[!]{str}\033[0m")
-
-def error(str):
-	print(f'-----------------------\
-----\033[0;31mErrors\033[0m----------------------\
------\n\t\033[0;31m{str}\033[0m\n\n-----------------------\
---------------------------------\
------')
-
-def pok(str):
-	print(f"[\033[0;32mOK\033[0m ] {str}")
-#TEST
-if __name__ == "__main__":
-	pout("sss")
-	pwarm("sss")
-	error("sss")
-	pok("sss")
