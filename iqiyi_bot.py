d0484d1a300cd5cbd9f99532f52e3582d22c65ba
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-06-02 22:46:55 +0800

    Update iqiyi_bot.py

--- iqiyi_bot.py
+++ iqiyi_bot.py
@@ -33,6 +33,7 @@ def __init__(self, full_url_str, filename):
     self.filename = filename
 
 
+# To ZZH:这边几个判断语句是什么屎山!!!!
 def download(self):
     '''''执行下载，返回True或False'''
 
@@ -45,67 +46,67 @@ def download(self):
 
         conn = None
 
-    if self.url.scheme == 'https':
+        if self.url.scheme == 'https':
 
-        conn = http.client.HTTPSConnection(self.url.netloc)
+            conn = http.client.HTTPSConnection(self.url.netloc)
+
+        else:
+
+            conn = http.client.HTTPConnection(self.url.netloc)
+
+            conn.request('GET', self.url.path)
+
+            response = conn.getresponse()
+
+        if response.status == 200:
+
+            total_size = response.getheader('Content-Length')
+
+            total_size = (int)(total_size)
+
+            if total_size > 0:
+
+                finished_size = 0
+
+            file = open(self.filename, 'wb')
+
+            if file:
+
+                progress = Progress()
+
+            progress.start()
+
+            while not response.closed:
+
+                buffers = response.read(1024)
+
+            file.write(buffers)
+
+            finished_size += len(buffers)
+
+            progress.update(finished_size, total_size)
+
+            if finished_size >= total_size:
+
+                break
+
+            # ... end while statment
+
+            file.close()
+
+            progress.stop()
+
+            progress.join()
+
+        else:
+
+            logging.error('Create local file %s failed' % (self.filename))
+
+            # ... end if statment
 
     else:
 
-        conn = http.client.HTTPConnection(self.url.netloc)
-
-        conn.request('GET', self.url.path)
-
-        response = conn.getresponse()
-
-    if response.status == 200:
-
-        total_size = response.getheader('Content-Length')
-
-        total_size = (int)(total_size)
-
-        if total_size > 0:
-
-        finished_size = 0
-
-        file = open(self.filename, 'wb')
-
-        if file:
-
-        progress = Progress()
-
-        progress.start()
-
-        while not response.closed:
-
-        buffers = response.read(1024)
-
-        file.write(buffers)
-
-        finished_size += len(buffers)
-
-        progress.update(finished_size, total_size)
-
-        if finished_size >= total_size:
-
-        break
-
-        # ... end while statment
-
-        file.close()
-
-        progress.stop()
-
-        progress.join()
-
-    else:
-
-        logging.error('Create local file %s failed' % (self.filename))
-
-        # ... end if statment
-
-    else:
-
-    logging.error('Request file %s size failed' % (self.filename))
+        logging.error('Request file %s size failed' % (self.filename))
 
     # ... end if statment
 
