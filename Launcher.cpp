09330f93606ae3bcb10df6a0aa6d37cbc2378863
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-06-21 22:32:43 +0800

    newer

--- Launcher.cpp
+++ Launcher.cpp
@@ -15,7 +15,7 @@ V1.0.0 Beta
 ***************************************/ 
 
 
-define SYSTEMFLAG "MSNT";
+#define SYSTEMFLAG "MSNT"
 //  在这里输入你的系统类型，"MSNT"代表微软
 //  的以NT为内核的系统 如Windows 7/8/10/11
 //  "*nix"代表类UNIX系统，包括但不限于UNIX、
@@ -23,17 +23,17 @@ define SYSTEMFLAG "MSNT";
 //  操作系统 
 
 
-if SYSTEMFLAG == "MSNT"						//NT内核系统？ 
-	define COMMAND  "ENV.exe \\bin\\bot.py"	//设置命令 
-	pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) // 设置入口地址
+#if SYSTEMFLAG == "MSNT"						//NT内核系统？ 
+	#define COMMAND  "ENV.exe \\bin\\bot.py"	//设置命令 
+	#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" ) // 设置入口地址
 
-elif SYSTEMFLAG == "*nix"						//类UNIX系统？ 
-	define COMMAND "python3 ./bin/bot.py"		//设置命令 
+#elif SYSTEMFLAG == "*nix"						//类UNIX系统？ 
+	#define COMMAND "python3 ./bin/bot.py"		//设置命令 
 
-else											//输入错误 
-	define COMMAND "0"							//返回异常 
+#else											//输入错误 
+	#define COMMAND "0"							//返回异常 
 
-endif
+#endif
 
 int main(){
 	
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # AutoDownloader_Bot
 一个解析并下载全网视频的爬虫.  
-# ***开发中***
-当前主要开发工作由团队核心成员ZZH在<https://gitcode.net/CN_ZZH/AutoDownloader_Bot>上进行
+***开发中***  
+当前主要开发工作由团队核心成员ZZH在<https://gitcode.net/CN_ZZH/AutoDownloader_Bot>上进行,但一切信息以Github上发布的为准.
