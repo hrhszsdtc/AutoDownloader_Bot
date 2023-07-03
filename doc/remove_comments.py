4c1dcef5665e3e25fd8c85020271cc249ed1091d
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-07-03 23:09:45 +0800

    Update remove_comments.py
    基本成型

--- doc/remove_comments.py
+++ doc/remove_comments.py
@@ -1,36 +1,38 @@
-import tkinter as tk
-from tkinter import ttk
+import os
 import re
+import tkinter as tk
+from tkinter import filedialog, ttk
 
 CHARS = {
-    "C": [r"/\*.*?\*/", r"//.*"],
-    "Python": [r"#.*", r"'''(.*?)'''", r'"""(.*?)"""'],
-    "Java": [r"/\*.*?\*/", r"//.*"],
+    "C": [r"/\*.*?\*/", r"//.*?"],
+    "Python": [r"#.*?", r"'''(?:.*?)'''", r'"""(?:.*?)"""'],
+    "Java": [r"/\*.*?\*/", r"//.*?"],
     "HTML": [r"<!--\s*[\S\s]*?\s*-->"],
     "CSS": [r"/\*\s*[\S\s]*?\s*\*/"],
-    "JavaScript": [r"/\*.*?\*/", r"//.*"],
-    "C++": [r"/\*.*?\*/", r"//.*"],
-    "Rust": [r"/\*.*?\*/", r"//.*"],
-    "Ruby": [r"#.*", r"=begin\s*([\S\s]*?)=end"],
-    "Scala": [r"/\*.*?\*/", r"//.*"],
-    "PHP": [r"/\*.*?\*/", r"//.*", r"#.*"],
-    "Perl": [r"#.*"],
-    "Go": [r"/\*.*?\*/", r"//.*"],
-    "Zig": [r"//.*", r"/\*.*?\*/"],
-    "Lua": [r"--\[\[\s*([\S\s]*?)\s*\]\]", r"--.*"],
-    "VBA": [r"'.*"],
-    "VBS": [r"'.*"],
-    "SQL": [r"--.*"],
-    "Kotlin": [r"/\*.*?\*/", r"//.*"],
-    "Swift": [r"/\*.*?\*/", r"//.*"],
-    "C#": [r"/\*.*?\*/", r"//.*"],
+    "JavaScript": [r"/\*.*?\*/", r"//.*?"],
+    "C++": [r"/\*.*?\*/", r"//.*?"],
+    "Rust": [r"/\*.*?\*/", r"//.*?"],
+    "Ruby": [r"#.*?", r"=begin\s*(?:[\S\s]*?)=end"],
+    "Scala": [r"/\*.*?\*/", r"//.*?"],
+    "PHP": [r"/\*.*?\*/", r"//.*?", r"#.*?"],
+    "Perl": [r"#.*?"],
+    "Go": [r"/\*.*?\*/", r"//.*?"],
+    "Zig": [r"//.*?", r"/\*.*?\*/"],
+    "Lua": [r"--\[\[\s*(?:[\S\s]*?)\s*\]\]", r"--.*?"],
+    "VBA": [r"'.*?"],
+    "VBS": [r"'.*?"],
+    "SQL": [r"--.*?"],
+    "Kotlin": [r"/\*.*?\*/", r"//.*?"],
+    "Swift": [r"/\*.*?\*/", r"//.*?"],
+    "C#": [r"/\*.*?\*/", r"//.*?"],
 }
+
 SUFFIX = "_nocomment"
 
 
 class Remover:
     def remove(self, file, chars):
-        with open(file, "r") as f:
+        with open(file, "r", encoding="utf-8") as f:
             content = f.read()
         for pattern in chars:
             content = re.sub(pattern, "", content, flags=re.DOTALL | re.MULTILINE)
@@ -38,15 +40,14 @@ class Remover:
 
     def select_file(self):
         global chosen_file
-        chosen_file = tk.filedialog.askopenfilename()
+        chosen_file = filedialog.askopenfilename()
 
-    def select_chars(self):
-        global chosen_chars
-
-    def remove_and_save(self):
+    def remove_and_save(self, chosen_chars):
         if chosen_file:
             new_file_content = self.remove(chosen_file, chosen_chars)
-            with open(chosen_file + SUFFIX, "w") as f:
+            file_name, file_ext = os.path.splitext(chosen_file)
+            new_file_name = file_name + SUFFIX + file_ext
+            with open(new_file_name, "w", encoding="utf-8") as f:
                 f.write(new_file_content)
         else:
             print("请选择一个文件")
@@ -55,6 +56,7 @@ class Remover:
 class GUI(tk.Frame, Remover):
     def __init__(self, master=None):
         super().__init__(master)
+        self.option_value = None
         self.master.title("Comment Remover")
         self.pack()
         self.create_widgets()
@@ -66,10 +68,16 @@ class GUI(tk.Frame, Remover):
         self.combobox["values"] = list(CHARS.keys())
         self.combobox.current(0)
         self.combobox.pack()
+        self.button_remove = tk.Button(
+            self,
+            text="移除注释",
+            command=lambda: (self.get_lang(), self.remove_and_save(self.option_value)),
+        )
+        self.button_remove.pack()
 
-    def update_lang(self):
+    def get_lang(self):
         option_key = self.combobox.get()
-        option_value = CHARS.get(option_key)
+        self.option_value = CHARS.get(option_key)
 
 
 if __name__ == "__main__":
