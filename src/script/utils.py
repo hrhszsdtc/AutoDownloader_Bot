3c020f11bc6629f7892b288e121bf14ef7e8d3ad
CoderWharf <139237018+CoderWharf@users.noreply.github.com>
2023-07-14 15:57:31 +0800

    Add Js Beautifier

---
+++ src/script/utils.py
@@ -0,0 +1,7 @@
+import jsbeautifier
+
+def Beautify_Js(input_js):
+    options = jsbeautifier.defalt_options()
+    formatted_js = jsbeautifier_beautify(input_js, options)
+    return formatted_js
+
