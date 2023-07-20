18778720766629222d85dee65ae31fb441530588
CoderWharf <139237018+CoderWharf@users.noreply.github.com>
2023-07-20 21:21:58 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -16,10 +16,11 @@ copyright_notice = "Copyright (C) 2023 hrhszsdtc"
 # 反馈事项
 feedback_notice = "请联系hrhszdtc团队(https://github.com/hrhszsdtc)并发送反馈报告,报告应包含:\n1. 使用的程序和运行环境\n2. 系统版本\n3. 报错信息\n4. 报错前的操作\n5. 其他可能的信息\n感谢您的反馈!"
 
-script_path = os.path.dirname(__file__)
-script_folder = os.path.basename(script_path)
-if script_folder is "src":
-    os.chdir(script_folder)
+script_path = os.path.basename(__file__)
+script_folder = os.path.dirname(script_path)
+script_name = os.path.basename(script_folder)
+if script_name == "src":
+    pass
 else:
     src_path = os.path.join(script_path, "src")
     if os.path.exists(src_path):
